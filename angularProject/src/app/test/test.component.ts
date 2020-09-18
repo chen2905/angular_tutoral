a1dcd160cd5932e20bf2930268a65c2d35db63b6
chen gao <c.gao@ecj.com.au>
2020-09-18 16:49:19 +1000

    fri

--- angularProject/src/app/test/test.component.ts
+++ angularProject/src/app/test/test.component.ts
@@ -2,14 +2,38 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  templateUrl: './test.component.html',
-  styleUrls: ['./test.component.css']
+  template: `
+  heroname:<input [disabled]="disabled" id='{{myID}}' type='text' value='spiderman' [class]="successClass"/>
+  <h2 [class.text-danger] ="hasError">{{name}}</h2>
+  `,
+  styles:[
+    `
+    .text-success{
+      color:green;
+    }
+    .text-danger{
+      color:red;
+    }
+    .text-special{
+      front-style:italic;
+    }
+    `
+  ]
 })
 export class TestComponent implements OnInit {
 
+  public name ="Mighty God"
+  public myID="hero1"
+  public disabled= true
+  public successClass="text-success"
+  public hasError = true
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  greetUser=()=>{
+    return this.name +"! I am your follower!"
   }
 
 }
